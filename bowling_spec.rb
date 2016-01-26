3bc977114db6c7894fedaec279f00da8d4742aa3
Lovinder Pnag <lspnag@gmail.com>
2016-01-26 01:53:05 -0500

    gutter game test

---
+++ bowling_spec.rb
@@ -0,0 +1,14 @@
+require_relative 'bowling'
+
+describe 'BowlingGame' do
+  before(:each) do
+    @game = Game.new
+  end
+
+  describe 'gutter game' do
+    it 'results in 0' do
+      @game.roll(0)
+      expect(@game.score).to eq 0
+    end
+  end
+end
