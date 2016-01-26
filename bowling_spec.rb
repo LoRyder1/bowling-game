e088d143fc233e1dded2a44102223f851f124302
Lovinder Pnag <lspnag@gmail.com>
2016-01-26 02:30:29 -0500

    test for one spare in game

--- bowling_spec.rb
+++ bowling_spec.rb
@@ -18,4 +18,12 @@ describe 'BowlingGame' do
     roll_many(20,1)
     expect(@game.score).to eq 20
   end
+
+  it 'test one spare' do
+    @game.roll(5)
+    @game.roll(5)
+    @game.roll(3)
+    roll_many(17,0)
+    expect(@game.score).to eq 15
+  end
 end
