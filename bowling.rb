5820ab37e1764f5c2a7c52e5f8c912e39f8b0fb8
Lovinder Pnag <lspnag@gmail.com>
2016-01-26 03:11:22 -0500

    refactor

--- bowling.rb
+++ bowling.rb
@@ -1,10 +1,24 @@
 class BowlingGame
-  attr_reader :score
+  attr_reader :rolls
   def initialize
     @score = 0
+    @rolls = []
+    @current_roll = 0
   end
 
-  def roll points
-    @score += points
+  def roll pins
+    @rolls.push(pins)
+  end
+
+  def score
+    @rolls.inject { |sum, x| sum + x}
   end
 end
+
+x = BowlingGame.new
+
+# x.roll(1)
+# x.roll(2)
+# x.roll(10)
+# p x.score
+# p x.rolls
